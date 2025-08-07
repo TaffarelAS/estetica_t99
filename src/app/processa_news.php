32b8dd6495e17b53faa798dd2bb6b3cf5467eddc
docker <docker@docker.my.domain>
2025-08-07 10:48:16 -0400

    07/08/2025

--- src/app/processa_news.php
+++ src/app/processa_news.php
@@ -23,14 +23,23 @@
     #echo "<h3>" . $email . "</h3>";
     #echo '<h3>' . $email . '</h3>';
 
-    # Conectando ao Banco de Dados via PDO()
+    # 1. Conectando ao Banco de Dados via PDO()
     $conexao = new PDO ( "mysql:host=db;dbname=estetica_t99;port=3306","root","root" );
 
-    sql_cria_tabela = "CREATE TABLE IF NOT EXISTS news 
-    ( id_news INT(255) NOT NULL DEFAULT AUTO_INCREMENT PRIMARY KEY,
-     email VARCHAR(254) NOT NULL UNIQUE,
-     validado INT(1) NOT NULL DEFAULT 0,
-     data_criacao DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    # 2. Montar a instrução SQL
+    $sql_cria_tabela = "CREATE TABLE IF NOT EXISTS estetica_t99.news 
+    ( 
+        id_news INT(255) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR(254) NOT NULL UNIQUE,
+        validado INT(1) NOT NULL DEFAULT 0,
+        opt_out ENUM ('sim','não') NOT NULL DEFAULT 'sim',
+        data_criacao DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
     )";
 
+    $sql_insere_email = "INSERT INTO estetica_t99.news ( email )
+    VALUES ( ? )";
+
+    # 3. Executar o comando atraves da conexão
+    $roda = $conexao -> prepare ($sql_insere_email);
+    $roda -> execute ( [$email] );
 ?>
